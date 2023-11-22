780c9c2ad02c15275431637c8469d34feab20156
platon777 <wichleyvalentin@gmail.com>
2023-11-21 19:28:02 -1100

    removing the venv folder and rename file to lowercase

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wvalentin\Documents\GitHub\Kerberos_project\venv
+set VIRTUAL_ENV=D:\project\Kerberos_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
